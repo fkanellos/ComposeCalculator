49179fbd565a960b0fb231a6b601e75ceb8e6de2
filippos.kanellos <filippos.kanellos@accenture.com>
2022-07-22 19:10:50 +0300

    Add existing project files prior to the push to GitHub.

---
+++ app/src/main/java/com/example/composecalculator/CalculatorAction.kt
@@ -0,0 +1,13 @@
+package com.example.composecalculator
+
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    object Divide: CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+    data class  Operation(val operation: CalculatorOperation): CalculatorAction()
+
+
+}
